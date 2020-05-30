31e6a252d9d375388b806d6787da9fefbf8bddbe
mahns <mahns1201@gmail.com>
2020-05-30 23:07:52 +0900

    어플리케이션 동기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'package:planbreaker/widget/WiseSaying.dart';
 import 'package:planbreaker/widget/Bottombar.dart';
-import 'package:planbreaker/screen/home_screen.dart';
+//import 'package:planbreaker/screen/home_screen.dart'; 명언 출력 if문으로 !
 import 'package:planbreaker/screen/letter_screen.dart';
 import 'package:planbreaker/screen/setting_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TabController controller;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+        home: HomeWidget()
+    );
+  }
+}
+
+class AppState {
+  bool loading;
+  FirebaseUser user;
+  AppState(this.loading, this.user);
+}
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  final app = AppState(false, null);
+  @override
+  Widget build(BuildContext context) {
+    if (app.loading) return _loading();
+    if (app.user == null) return _logIn();
+    return _main();
+  }
+  Widget _loading () {
+    return Scaffold(
+        appBar: AppBar(title: Text('loading...')),
+        body: Center(child: CircularProgressIndicator())
+    );
+  }
+  Widget _logIn () {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('일정뽀시기')
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                    child: Text('google ID로 로그인'),
+                    onPressed: () {
+                      _signIn();
+                    }
+                )
+              ],
+            )
+        )
+    );
+  }
+  Widget _main () {
     return MaterialApp(
       title: 'PlanBreaker',
       theme: ThemeData(
@@ -26,10 +78,10 @@ class _MyAppState extends State<MyApp> {
           body: TabBarView(
             physics: NeverScrollableScrollPhysics(),
             children: <Widget>[
-              HomeScreen(),
+              WiseSaying(),    // 나중에 파이차트 부분이 구현완료 되면, if문으로 호출! (home screen 자리)
               Container(child: Center(child: Text('달성기록'))),
               Lettere(),
-              MyAp(),
+              Setting(),
             ],
           ),
           bottomNavigationBar: Bottom(),
@@ -37,33 +89,39 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> _signIn() async {
+    setState(() => app.loading = true);
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+    await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
+    print(user);
+
+    setState(() {
+      app.loading = false;
+      app.user = user;
+    });
+
+    return 'success';
+  }
+  /* 로그아웃 환경설정에 추가?
+  void _signOut() async{
+    await googleSignIn.signOut();
+    // await _auth.signOut();
+    setState(() {
+      app.user = null;
+    });
+  }
+  */
 }
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//     Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'PlanBreaker',
-//       theme: ThemeData(
-//           brightness: Brightness.dark,
-//           primaryColor: Colors.black,
-//           accentColor: Colors.white),
-//       home: DefaultTabController(
-//         length: 4,
-//         child: Scaffold(
-//           body: TabBarView(
-//             physics: NeverScrollableScrollPhysics(),
-//             children: <Widget>[
-//               HomeScreen(),
-//               Container(child: Center(child: Text('달성기록'))),
-//               Lettere(),
-//               MyAp(),
-//             ],
-//           ),
-//           bottomNavigationBar: Bottom(),
-//         ),
-//       ),
-//     );
-//   }
-// }
--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+//import 'package:planbreaker/screen/login_main.dart'; 메소드만 호출하는 법?
 
-class MyAp extends StatefulWidget {
-  MyApState createState() => MyApState();
+class Setting extends StatefulWidget {
+  SettingState createState() => SettingState();
 }
 
-class MyApState extends State<MyAp> {
+class SettingState extends State<Setting> {
   bool toggleValue = false;
 
   toggleButton() {
@@ -16,40 +17,52 @@ class MyApState extends State<MyAp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: AnimatedContainer(
+      appBar: AppBar(
+        title: Text("환경설정"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              // 로그아웃(?)
+            },
+          )
+        ],
+      ),  
+      body: Center(
+        child: AnimatedContainer(
         duration: Duration(milliseconds: 500),
         height: 40.0,
         width: 100.0,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20.0),
-            color: toggleValue
-                ? Colors.greenAccent[100]
-                : Colors.redAccent[100].withOpacity(0.5)),
-        child: Stack(children: <Widget>[
-          AnimatedPositioned(
-            duration: Duration(milliseconds: 500),
-            curve: Curves.easeIn,
-            top: 3.0,
-            left: toggleValue ? 60.0 : 0.0,
-            right: toggleValue ? 0.0 : 60.0,
-            child: InkWell(
-              onTap: toggleButton,
-              child: AnimatedSwitcher(
-                duration: Duration(milliseconds: 500),
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return RotationTransition(child: child, turns: animation);
-                },
-                child: toggleValue
-                    ? Icon(Icons.check_circle,
-                        color: Colors.green, size: 35.0, key: UniqueKey())
-                    : Icon(Icons.remove_circle_outline,
-                        color: Colors.red, size: 35.0, key: UniqueKey()),
+          borderRadius: BorderRadius.circular(20.0),
+          color: toggleValue
+              ? Colors.greenAccent[100]
+              : Colors.redAccent[100].withOpacity(0.5)
               ),
+      child: Stack(children: <Widget>[
+        AnimatedPositioned(
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeIn,
+          top: 3.0,
+          left: toggleValue ? 60.0 : 0.0,
+          right: toggleValue ? 0.0 : 60.0,
+          child: InkWell(
+            onTap: toggleButton,
+            child: AnimatedSwitcher(
+              duration: Duration(milliseconds: 500),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return RotationTransition(child: child, turns: animation);
+              },
+              child: toggleValue
+                  ? Icon(Icons.check_circle,
+                      color: Colors.green, size: 35.0, key: UniqueKey())
+                  : Icon(Icons.remove_circle_outline,
+                      color: Colors.red, size: 35.0, key: UniqueKey()),
             ),
-          )
-        ]),
-      ),
-    ));
+          ),
+        )
+      ]),
+     ),
+   ));
   }
 }
